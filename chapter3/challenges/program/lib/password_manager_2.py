b38c38276727c410a22cda8222e3e5d15b3f1d8e
JakeOtto <jake_zimmermann@hotmail.co.uk>
2023-09-14 21:35:15 +0100

    chapter 3 challenge

--- chapter3/challenges/program/lib/password_manager_2.py
+++ chapter3/challenges/program/lib/password_manager_2.py
@@ -69,36 +69,99 @@
 
 from datetime import datetime
 class PasswordManager2():
-    pass
-
-def __init__(self):
-    self.password_dic = {}
     
-def add(self, ser_name, ser_pass):
-    
-    valid = False
-    req_char = ["!", "@", "$", "%", "&"]
+    # constructor - self  
+    def __init__(self):
+        self.password_dict = {}
 
-    if len(ser_pass) >= 8 and any (char in ser_pass for char in req_char):
-        for key, value in self.password_dic.items():
-            if value != ser_pass:
+    # add  - self, ser_name, ser_pass
+    def add(self,ser_name,ser_pass):
+        time = datetime.today()
+        valid = False
+        req_char = ["!","@","$","%","&"]
+    
+        if len(ser_pass) >= 8 and any (char in ser_pass for char in req_char):
+            if bool(self.password_dict) == False:
                 valid = True
 
-    if valid:
-        self.password_dic[ser_name]=ser_pass
+            else:
+                password_list=[]
+                for info in self.password_dict.values():
+                    password_list.append(info["password"])
+                    if ser_pass not in password_list:
+                        valid = True
+            
+        if valid:
+            self.password_dict[ser_name]={}
+            self.password_dict[ser_name]["password"]=ser_pass
+            self.password_dict[ser_name]["added_on"]=time
 
-def get_for_service(self, ser_name):
+    # remove  - self, ser_name
+    def remove(self, ser_name):
+        del self.password_dict[ser_name]
+
+    # update - self, ser_name
+    def update(self, ser_name,ser_pass):
+        time = datetime.today()
+        valid = False
+        req_char = ["!","@","$","%","&"]
     
-    for key, value in self.password_dic.items():
-        if key == ser_name:
-            return value
+        if len(ser_pass) >= 8 and any (char in ser_pass for char in req_char):
+            if bool(self.password_dict) == False:
+                valid = True
+
+            else:
+                password_list=[]
+                for info in self.password_dict.values():
+                    password_list.append(info["password"])
+                if ser_pass not in password_list:
+                    valid = True
+            
+        if valid:
+            self.password_dict[ser_name]={}
+            self.password_dict[ser_name]["password"]=ser_pass
+            self.password_dict[ser_name]["added_on"]=time
+
+    # list services - self
+    def list_services(self):
+        keys_list=[]
+        for key, value in self.password_dict.items():
+            if value != None:
+                keys_list.append(key)
+        return keys_list
+
+    # get for service - self , ser_name
+    def get_for_service(self, ser_name):
+        for key, value in self.password_dict.items():
+            if key == ser_name:
+                return self.password_dict[ser_name]["password"]
+        else:
+            return None
         
-    return None
 
-def list_services(self):
-    keys_list = []
-    for key, value in self.password_dic.items():
-        if value != None:
-            keys_list.append(key)
+    # sort services  - self, *args
+    def sort_services_by(self,*args):
+        if args == "service" or args == ('service',):
+            return sorted(self.password_dict)
+        
+        elif args == ("service", "reverse"):
+            sorted_list = sorted(self.password_dict)
+            sorted_list.reverse()
+            return sorted_list
+        
+        elif args == "added_on" or args == ('added_on',):
+            sorted_tuple_list = sorted(self.password_dict.items(), key = lambda x: x[1]["added_on"])
+            sorted_key_list = []
+            for i in sorted_tuple_list:
+                sorted_key_list.append(i[0])
+            return sorted_key_list
+            
+        elif args == ("added_on", "reverse"):
+            sorted_tuple_list = sorted(self.password_dict.items(), key = lambda x: x[1]["added_on"])
+            sorted_key_list = []
+            for i in sorted_tuple_list:
+                sorted_key_list.append(i[0])
+            sorted_key_list.reverse()
+            return sorted_key_list
+        
 
-    return keys_list
